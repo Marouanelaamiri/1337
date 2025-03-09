c6daecc3f7e19e6b1a1f756e9ff9b744bd557c8a
Marouane <maroaunelaamiri8@gmail.com>
2025-03-09 17:21:48 +0000

    Philo

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,7 +6,7 @@
 /*   By: malaamir <malaamir@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:25:10 by malaamir          #+#    #+#             */
-/*   Updated: 2025/03/08 20:40:06 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/03/09 17:21:00 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +40,9 @@ int check_valid_num(char *str)
 }
 int check_valid_args(char **av)
 {
-	// int philo_count;
-
-	// philo_count = ft_atoi(av[1]);
 	if (ft_atoi(av[1]) > PHILO_MAX || ft_atoi(av[1]) <= 0
 	|| check_valid_num(av[1]) == 1)
 		return (write(2, "Error: Invalid number of philosophers\n", 38), 1);
-	// if (philo_count == 1)
-		// return (write(2, "Error: Invalid number of philosophers\n", 38), 1);
 	if (ft_atoi(av[2]) <= 0 || check_valid_num(av[2]) == 1)
 		return (write(2, "Error: Invalid time to die\n", 27), 1);
 	if (ft_atoi(av[3]) <= 0 || check_valid_num(av[3]) == 1)
